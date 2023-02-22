0ea00bd6a4d50e0d4d6483660a91bc060a9ec9de
uriuribo <rokkouribou@gmail.com>
2023-02-22 21:37:02 +0900

    回答されたファイルを探索する(読込めないエラーを直す)

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const correct_answer = document.getElementById("formFile");
 const answers = document.getElementById("formFileMultiple");
+
+// 名前と点数を保存
+var scores = [{}]
+
 document.getElementById("scoring").onclick = function () {
-  // console.log(correct_answer.files);
   // console.log(correct_answer.files[0]["name"]);
-  console.log(answers.files);
+
   const reader = new FileReader();
   var cset = new Set()
 
+  // 正答のファイルを読み込み
   reader.readAsText(correct_answer.files[0]);
-  // console.log(correct_answer.files[0]);
 
   reader.onload = function(e){
     var correct_answers = reader.result.split(/\r\n|\n/);
-    // console.log(correct_answers)
+
     for(let i = 0;i < correct_answers.length;i++){
       cset.add(correct_answers[i])
     }
-    console.log(cset)
   };
 
-  for(let i = 0;i < answers.length;i++){
-    console.log(answers.files[i])
+
+  // 回答者たちの答えを読み込みしたいがエラー(同期ができていない？)
+  for(let i = 0;i < answers.files.length;i++){
+    reader.readAsText(answers.files[i]);
+    reader.onload = function(e){
+      console.log(reader.result)
+    }
   }
 
 };
