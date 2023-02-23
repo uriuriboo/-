1beeb7e794113cc23270bfad39aacaa2a25e11f1
uriuribo <rokkouribou@gmail.com>
2023-02-23 15:29:53 +0900

    関数の同期がうまくいかない

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ const correct_answer = document.getElementById("formFile");
 const answers = document.getElementById("formFileMultiple");
 
 // 名前と点数を保存
-var scores = [{}]
+var scores = {}
 
-function answer_parser(file){
+async function answer_parser(file){
   const reader = new FileReader();
   var set = new Set()
+
   reader.readAsText(file);
-
-  reader.onload = function(e){
-    var answers = reader.result.split(/\r\n|\n/);
-
-    for(let i = 0;i < answers.length;i++){
-      set.add(answers[i])
-    }
+  reader.onload =  await function(e){
+    set = new Set(reader.result.split(/\r\n|\n/))
   }
-  // サイズが0になる
   console.log(set)
   return set;
 };
 
 document.getElementById("scoring").onclick = function () {
-  var cset = answer_parser(correct_answer.files[0]);
+  var cset = await answer_parser(correct_answer.files[0]);
+
   for (let i = 0; i < answers.files.length; i++){
-    var aset = answer_parser(answers.files[i]);
+    var aset = await answer_parser(answers.files[i]);
+    // 以下で点数の採点を行い、連想配列scoresにファイルの名前から取った回答者名と点数を記録
+
   }
+  // htmlに順位、名前、点数を表示
 }
